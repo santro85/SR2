575f108e514d05b5390f64ce02bd00139693b392
santro85 <45917354+santro85@users.noreply.github.com>
2019-01-11 10:57:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# SR2
-Sound Recorder using recorder.js
+#  Sample Code for HTML5 MediaRecorder API usage
+
+-------
+
+### [demo](https://mido22.github.io/MediaRecorder-sample/)
+
+-------
+
+### Works on:
+* Chrome: v47+
+* Firefox: v25+
---
+++ SW.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'SoundRecorder';
+//new
+var urlsToCache = [
+  '/',
+  'style.css',
+  'app.js',
+  'sound-recorder-icon.png',
+  'bootstrap.min.js',
+  'bootstrap.min.css',
+ 'jquery-2.2.0.min.js'
+];
+
+
+self.addEventListener('install', function (event) {
+  console.log("installing")
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache).then(function () {
+          console.log('All resources have been fetched and cached.');
+        });
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+
+    })
+  );
+
+});
