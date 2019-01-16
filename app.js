2d732993373b92d72f3d859e99776c4fb320b4d7
santro85 <45917354+santro85@users.noreply.github.com>
2019-01-16 14:30:09 +0530

    adding Web APK functionality

--- app.js
+++ app.js
@@ -166,3 +166,25 @@ function createDownloadLink(blob) {
 	//add the li element to the ol
 	recordingsList.appendChild(li);
 }
+
+
+var installButton = document.getElementById('install');
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('safety-worker.js').then(() => {
+    return navigator.serviceWorker.ready;
+  }).then(reg => {
+    console.log('Service Worker ready :^)', reg);
+  }).catch(error => {
+    console.log('Service Worker error :^(', error);
+  });
+  window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault();
+    // console.log('beforeinstallprompt event:', event);
+    installButton.removeAttribute('hidden');
+    installButton.addEventListener('click', () => {
+      event.prompt();
+      installButton.setAttribute('disabled', true);
+    });
+  });
+}
